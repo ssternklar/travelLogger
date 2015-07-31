220685d8d5af62faf66ca90c94473e4f08e3f26e
amritharam <amritha119@gmail.com>
2015-08-01 00:10:02 +0530

    ShareActionProvider

--- app/src/main/java/com/example/android/travellogger/DetailActivity.java
+++ app/src/main/java/com/example/android/travellogger/DetailActivity.java
@@ -1,13 +1,9 @@
 package com.example.android.travellogger;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.google.android.gms.plus.PlusShare;
 
 
 public class DetailActivity extends ActionBarActivity {
@@ -37,18 +33,18 @@ public class DetailActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-        if(id==R.id.action_share) {
-//            Intent intent = new Intent(DetailActivity.this, PlusActivity.class);
-//            startActivity(intent);
-
-            Intent shareIntent = new PlusShare.Builder(this)
-                    .setType("text/plain")
-                    .setText("Welcome to the Google+ platform.")
-                    .setContentUrl(Uri.parse("https://developers.google.com/+/"))
-                    .getIntent();
-
-            startActivityForResult(shareIntent, 0);
-        }
+//        if(id==R.id.Google) {
+////            Intent intent = new Intent(DetailActivity.this, PlusActivity.class);
+////            startActivity(intent);
+//
+//            Intent shareIntent = new PlusShare.Builder(this)
+//                    .setType("text/plain")
+//                    .setText("Welcome to the Google+ platform.")
+//                    .setContentUrl(Uri.parse("https://developers.google.com/+/"))
+//                    .getIntent();
+//
+//            startActivityForResult(shareIntent, 0);
+//        }
 
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/com/example/android/travellogger/DetailActivityFragment.java
+++ app/src/main/java/com/example/android/travellogger/DetailActivityFragment.java
@@ -3,7 +3,13 @@ package com.example.android.travellogger;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -15,6 +21,7 @@ import android.widget.TextView;
 public class DetailActivityFragment extends Fragment {
 
     public DetailActivityFragment() {
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -28,5 +35,30 @@ public class DetailActivityFragment extends Fragment {
         titleTextView.setText(postTitle);
         getActivity().setTitle(postTitle);
         return rootView;
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Retrieve the share menu item
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+
+        // Get the provider and hold onto it to set/change the share intent.
+        ShareActionProvider mShareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        // Attach an intent to this ShareActionProvider.  You can update this at any time,
+        // like when the user selects a new piece of data they might like to share.
+        if (mShareActionProvider != null ) {
+            mShareActionProvider.setShareIntent(createShareIntent());
+        } else {
+            Log.d(getActivity().getClass().getSimpleName(), "Share Action Provider is null?");
+        }
+    }
+    private Intent createShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+//        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,
+                "Test String!");
+        return shareIntent;
     }
 }
