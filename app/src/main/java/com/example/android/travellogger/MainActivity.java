4e970e928a8d8adf3c1f6ad2fc23ffba3ee5a35d
amritharam <amritha119@gmail.com>
2015-08-08 16:01:15 -0400

    Final logo.

--- app/src/main/java/com/example/android/travellogger/MainActivity.java
+++ app/src/main/java/com/example/android/travellogger/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
